54c03294b2649dd2147d71d7f9a7b1fbc58604ca
ahmedambadi <ahmedambadi.dev@gmail.com>
2025-09-16 18:29:20 +0530

    clean slate

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const usercontroller = require('../controllers/userController');
-const express = require('express');
-const router = express.Router();
-
-
-router.post('/register', usercontroller.CreateUser);
-router.post('/login', usercontroller.signin);
-
-module.exports = router;
